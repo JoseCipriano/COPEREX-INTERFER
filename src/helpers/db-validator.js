0808acf2cebb6233ec76ff7e5ec222c16b2806ce
josecipriano <ciprianoaceytunoj@gmail.com>
2025-03-04 20:57:01 -0600

    helpers

---
+++ src/helpers/db-validator.js
@@ -0,0 +1,20 @@
+import User from '../users/user.model.js';
+
+
+
+export const existenteEmail = async (correo = ' ') => {
+
+    const existeEmail = await User.findOne({ correo });
+
+    if(existeEmail){
+        throw new Error(`El correo ${ correo } ya existe en la base de datos`);
+    }
+}
+
+export const existeUsuarioById = async (id = '') => {
+    const existeUsuario = await User.findById(id);
+
+    if(!existeUsuario){
+        throw new Error(`El ID ${id} no existe`);
+    }
+}
